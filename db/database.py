6719f97418e88891aa784ffe810a112bc657c3f9
Amirthashree L <kavithaacsl@gmail.com>
2026-02-27 23:33:53 +0530

    Update model loader to use Hugging Face and fix database config

--- db/database.py
+++ db/database.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
+import os
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 client = MongoClient(MONGO_URL)
-
 db = client["phishing_detection_db"]
 
 print("✅ MongoDB Connected Successfully")
